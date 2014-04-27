0623959f5c6c5aeab292b4e19cc148d86cdc98a7
Rubens Mariuzzo <rubens@mariuzzo.com>
2014-04-27 19:29:40 -0400

    Workbench initialization.

---
+++ src/Rmariuzzo/LaravelJsLocalization/LaravelJsLocalizationServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Rmariuzzo\LaravelJsLocalization;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelJsLocalizationServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
