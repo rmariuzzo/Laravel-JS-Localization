91722fb90d8e383360f3c87497c37a803c78ecd9
Vagner do Carmo <vluzrmos@gmail.com>
2015-07-04 07:00:36 -0300

    improving lang.choice

--- tests/spec/lang_choice_spec.js
+++ tests/spec/lang_choice_spec.js
@@ -26,6 +26,16 @@ describe('The Lang.choice() method', function() {
         expect(Lang.choice('messages.plural', 10)).toBe('a million apples');
     });
 
+    it('should count the correctly', function() {
+        expect(Lang.choice('messages.count', 0)).toBe('There are none');
+        expect(Lang.choice('messages.count', 1)).toBe('There are some');
+        expect(Lang.choice('messages.count', 10)).toBe('There are some');
+        expect(Lang.choice('messages.count', 19)).toBe('There are some');
+        expect(Lang.choice('messages.count', 19)).toBe('There are some');
+        expect(Lang.choice('messages.count', 20)).toBe('There are many');
+        expect(Lang.choice('messages.count', 30)).toBe('There are many');
+    });
+
     it('should return the expected message with replacements', function() {
         expect(Lang.choice('validation.accepted', 1)).toBe('The :attribute must be accepted.');
         expect(Lang.choice('validation.accepted', 1, {
