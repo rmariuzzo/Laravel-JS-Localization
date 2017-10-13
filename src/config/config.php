ee41bf5ddbf603bbe2f1ae711bf4c5f81ee78f25
JBtje <jj__bb@hotmail.com>
2017-10-13 15:55:06 +0200

    Update config.php

--- src/config/config.php
+++ src/config/config.php
@@ -16,6 +16,19 @@ return [
     ],
 
     /*
+     * Set the name(s) a file must contain, for it to be included on the generated javascript.
+     * E.g. if you place all language files for JS in a seperate folder: lang/en/js/example.php, you can use:
+     *
+     * 'pathContains' => [
+            'js'
+     * ],
+     * Note: this method will check the occurrance of the name, in the full file path (including the file name, but not extension)
+     */
+    'pathContains' => [
+
+    ],
+
+    /*
      * The default path to use for the generated javascript.
      */
     'path' => public_path('messages.js'),
